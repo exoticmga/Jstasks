89b680f7134a820e7d27aa43d3cad038ea8778cd
exoticmga <agwumbaagha@gmail.com>
2022-06-21 04:29:58 +0100

    js

---
+++ index.js
@@ -0,0 +1,9 @@
+//Write a javascript code that prints out your name, height, and country on the screen.
+
+function my_details(name, height, country) {
+    var myName = "Mba Agha";
+    var myHeight = "7.8cm";
+    var myCountry = 'Nigeria';
+    alert("Here are my full details:  My name is : " + myName + "; My height is: " + myHeight + "; and My country is : " + myCountry + "");
+}
+my_details();
